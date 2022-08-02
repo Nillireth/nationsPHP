811ddae57404aab397718eb6c84833c408b7db92
Your Name <you@example.com>
2022-08-02 11:30:49 -0500

    primeros modelos

---
+++ app/Models/Country.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //La tabla a conectar a este modelo.
+    protected $table = "countries";
+
+    //La clave primaria de esa tabla.
+    protected $primaryKey = "country_id";
+
+    //omitir campos de auditoria.
+    public $timestamps = false;
+
+    use HasFactory;
+}
